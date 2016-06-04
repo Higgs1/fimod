faf42e90929f7d291e04077945506485e2726d7f
Sidney Keese <sidke.z@gmail.com>
2016-06-04 15:09:45 -0700

    sell all components on factory mod

---
+++ src/mods/clearfactories.js
@@ -0,0 +1,60 @@
+import Fimod from '../Fimod';
+
+import { insertStyle } from '../lib/utility';
+
+const style = `
+.controlsBox {
+  width: auto;
+  float: right;
+}
+
+.controlsBox .clearFactoryButton {
+  width: 110px;
+  border: 1px solid darkred;
+}
+`;
+
+const buttonTemplate = `
+<a id="clearFactory" class="button clearFactoryButton" href="javascript:void(0);">Clear factory</a>
+`;
+
+Fimod.define({
+  name: "clearfactories",
+  label: "Clear Factories",
+  description: "Provides a button to remove all buildings from a factory floor",
+},
+['game/Factory', 'game/action/SellComponentAction', 'ui/factory/ControlsUi', 'ui/helper/ConfirmUi'],
+(Factory, SellComponentAction, ControlsUi, ConfirmUi) => {
+  insertStyle(style);
+
+  Fimod.wrap(ControlsUi, 'display', function(supr, ...args) {
+    supr(...args);
+
+    const $container = this.container;
+    const $tracksButton = $('#clearPackages', $container);
+    const $factoryButton = $(buttonTemplate);
+    $tracksButton.after($factoryButton);
+
+    const sellComponents = () => {
+      this.factory.getTiles()
+        .filter(tile => tile.isMainComponentContainer())
+        .map(tile => {
+          const meta = tile.getComponent().meta;
+          return new SellComponentAction(tile, meta.width, meta.height);
+        })
+        .map(action => {
+          if (action.canSell()) action.sell();
+          console.log(action.canSell());
+        });
+    };
+
+    $factoryButton.click((_event) => {
+      const confirm = new ConfirmUi("Clear slot", "Are you sure you want to clear this factory?");
+      confirm
+        .setOkTitle("Cancel")
+        .setCancelTitle("Yes, clear factory")
+        .setCancelCallback(sellComponents)
+        .display();
+    });
+  });
+});
