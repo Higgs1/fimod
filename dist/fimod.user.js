0817b32eca55f05a984226fa4bfe3ed0e0b9b410
Sidney Keese <sidke.z@gmail.com>
2016-06-06 21:24:39 -0700

    bumped version

--- dist/fimod.user.js
+++ dist/fimod.user.js
@@ -4,7 +4,7 @@
 // @namespace   https://github.com/sidke/fimod
 // @description factory idle mod
 // @include     http://factoryidle.com/*
-// @version     1.06.9
+// @version     1.06.10
 // @grant       none
 // @run-at      document-start
 // ==/UserScript==
